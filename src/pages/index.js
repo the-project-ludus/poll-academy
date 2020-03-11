21b30d5979b308ee38e439f5484e4cd813a26118
Rubén <rubenofen@gmail.com>
2020-03-12 00:44:01 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,27 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "./index.scss"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Pregunta 4</h1>
+    <p>¿Con que ejercicios sientes que has aprendido más?</p>
+    <form
+      name="pregunta4"
+      method="post"
+      data-netlify="true"
+      data-netlyfy-honeypot="bot-field"
+    >
+      <textarea
+        name="pregunta4-respuesta"
+        id="pregunta4-respuesta"
+        cols="60"
+        rows="5"
+      ></textarea>
+      <button>Send</button>
+    </form>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
