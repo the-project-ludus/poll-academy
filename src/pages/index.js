44b8af13af05ef0ca3b1c07934e2d99928e23bef
Rubén <rubenofen@gmail.com>
2020-03-12 01:40:04 +0100

    fix submit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,9 @@ const IndexPage = () => (
       name="pregunta4"
       method="post"
       data-netlify="true"
-      data-netlify-honeypot="bot-field"
     >
-      <textarea
-        name="pregunta4-respuesta"
-        id="pregunta4-respuesta"
-        cols="60"
-        rows="5"
-      ></textarea>
-      <button>Send</button>
+      <textarea name="message"></textarea>
+      <button type="submit">Send</button>
     </form>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
