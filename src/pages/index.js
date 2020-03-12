8ab5369aec72cb859e4350e44315b02a7a0d8f39
rubenofen <rubenofen@gmail.com>
2020-03-12 09:23:42 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,21 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Pregunta 4</h1>
     <p>¿Con que ejercicios sientes que has aprendido más?</p>
-    <form
-      className="test-form"
-      name="pregunta4"
-      method="POST"
-      data-netlify="true"
-    >
-      <label>
-        Respuesta:<textarea name="message"></textarea>
-      </label>
-      <button type="submit">Send</button>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label style="font-family: monospace;">Name<br><input style="width: 100%;" type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label style="font-family: monospace;">Email<br><input style="width: 100%;" type="email" name="email" /></label>
+      </p>
+      <p>
+        <label style="font-family: monospace;">Message<br><textarea style="width: 100%;" rows="15" name="message"></textarea></label>
+      </p>
+      <div data-netlify-recaptcha="true"></div>
+      <br>
+      <p>
+        <button class="send-button" type="submit">Send</button>
+      </p>
     </form>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
