5c414c9c65a757372bd56727aabae4c291633b62
dastasoft <dastasoft@protonmail.com>
2020-03-15 00:05:32 +0100

    styled component added

--- src/components/questions/Pregunta0.js
+++ src/components/questions/Pregunta0.js
@@ -1,51 +1,45 @@
-import React, { useContext, useState } from "react"
-import axios from "axios"
-import UserContext from "../../context/UserContext"
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import UserContext from '../../context/UserContext';
+
+import Styles from './Styles';
 
 const Pregunta0 = ({ onSubmit }) => {
-  const [nombre, setNombre] = useState("")
-  const [email, setEmail] = useState("")
-  const { user, setUser } = useContext(UserContext)
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const { user, setUser } = useContext(UserContext);
 
   const handleSubmit = async e => {
-    e.preventDefault()
-    setUser(email)
-    console.log("user", user)
+    e.preventDefault();
+    setUser(email);
+    console.log('user', user);
     await axios.post(
-      "https://the-project-ludus-poll.firebaseio.com/questions.json",
+      'https://the-project-ludus-poll.firebaseio.com/questions.json',
       {
-        pregunta: "pregunta0",
+        pregunta: 'pregunta0',
         nombre: nombre,
-        email: email,
+        email: email
       }
-    )
-    onSubmit()
-  }
+    );
+    onSubmit();
+  };
 
-  const handleNombreChange = ev => setNombre(ev.target.value)
-  const handleEmailChange = ev => setEmail(ev.target.value)
+  const handleNombreChange = ev => setNombre(ev.target.value);
+  const handleEmailChange = ev => setEmail(ev.target.value);
 
   return (
-    <div>
+    <Styles>
       <h1>Pregunta 0</h1>
       <p>¿Con que ejercicios sientes que has aprendido más?</p>
       <form className="test-form">
-        <label>
-          Nombre y apellidos:
-          <input
-            type="text"
-            name="nombre"
-            onChange={handleNombreChange}
-          ></input>
-        </label>
-        <label>
-          E-mail:
-          <input type="text" name="email" onChange={handleEmailChange}></input>
-        </label>
+        <label>Nombre y apellidos:</label>
+        <input type="text" name="nombre" onChange={handleNombreChange}></input>
+        <label>E-mail:</label>
+        <input type="text" name="email" onChange={handleEmailChange}></input>
         <button onClick={handleSubmit}>Send</button>
       </form>
-    </div>
-  )
-}
+    </Styles>
+  );
+};
 
-export default Pregunta0
+export default Pregunta0;
---
+++ src/components/questions/Styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  margin: 0 auto;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  padding: 21px;
+  display: grid;
+
+  form {
+    display: grid;
+    grid-template-columns: 9em auto;
+    grid-gap: 1em;
+    margin: 0;
+  }
+
+  input {
+    max-width: 300px;
+  }
+
+  button {
+    width: 100%;
+    max-width: 158px;
+    justify-self: flex-end;
+    grid-column: 2/3;
+  }
+`;
+
+export default Styles;
