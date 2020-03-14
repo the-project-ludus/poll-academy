bbb92d06a9cbeb814642af8ad299fcf8537fb68e
Rubén <rubenofen@gmail.com>
2020-03-15 00:45:19 +0100

    Pregunta 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import useQuestions from "../hooks/useQuestions"
-import UserContext from "../context/UserContext"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import useQuestions from '../hooks/useQuestions';
+import UserContext from '../context/UserContext';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import "./index.css"
+import './index.css';
 
 const IndexPage = () => {
-  const [questionId, setQuestionId] = useState(0)
-  const [user, setUser] = useState("")
-  const questionComponents = useQuestions(() => setQuestionId(questionId + 1))
+  const [questionId, setQuestionId] = useState(1);
+  const [user, setUser] = useState('');
+  const questionComponents = useQuestions(() => setQuestionId(questionId + 1));
 
   const llamada = () =>
-    axios.post("https://the-project-ludus-poll.firebaseio.com/questions.json", {
-      nombre: "antonio",
-    })
+    axios.post('https://the-project-ludus-poll.firebaseio.com/questions.json', {
+      nombre: 'antonio'
+    });
 
   return (
     <Layout>
@@ -26,7 +26,7 @@ const IndexPage = () => {
       </UserContext.Provider>
       <button onClick={llamada}>Llamada</button>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
