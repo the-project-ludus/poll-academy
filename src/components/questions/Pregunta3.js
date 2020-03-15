f01ec905d2249e5e0786ef34d11026a16e315f27
Rubén <rubenofen@gmail.com>
2020-03-15 01:06:00 +0100

    Pregunta 3

---
+++ src/components/questions/Pregunta3.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import topicList from '../topicList';
+import useCheckboxListQuestions from '../../hooks/useCheckboxListQuestions';
+
+const Pregunta3 = ({ onSubmit }) => {
+  const [handleSubmit, handleCheboxChange] = useCheckboxListQuestions(
+    'pregunta3',
+    onSubmit
+  );
+
+  return (
+    <div>
+      <h1>Pregunta 3</h1>
+      <p>Marca los temas donde has echado de menos más ejercicios.</p>
+      <form className="test-form" name="pregunta3">
+        <ul>
+          {topicList.map(topic => (
+            <li key={topic.id}>
+              <input
+                type="checkbox"
+                name={topic.description}
+                onChange={handleCheboxChange}
+              />
+              {topic.description}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleSubmit}>Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Pregunta3;
--- src/hooks/useQuestions.js
+++ src/hooks/useQuestions.js
@@ -1,13 +1,21 @@
-import React from "react"
-import Pregunta0 from "../components/questions/Pregunta0"
-import Pregunta1 from "../components/questions/Pregunta1"
-import Pregunta4 from "../components/questions/Pregunta4"
+import React from 'react';
+import Pregunta0 from '../components/questions/Pregunta0';
+import Pregunta1 from '../components/questions/Pregunta1';
+import Pregunta2 from '../components/questions/Pregunta2';
+import Pregunta3 from '../components/questions/Pregunta3';
+import Pregunta4 from '../components/questions/Pregunta4';
 
-const questions = [<Pregunta0 />, <Pregunta1 />, <Pregunta4 />]
+const questions = [
+  <Pregunta0 />,
+  <Pregunta1 />,
+  <Pregunta2 />,
+  <Pregunta3 />,
+  <Pregunta4 />
+];
 
 const useQuestions = onSubmit =>
   questions.map(question =>
     React.cloneElement(question, { onSubmit: onSubmit })
-  )
+  );
 
-export default useQuestions
+export default useQuestions;
