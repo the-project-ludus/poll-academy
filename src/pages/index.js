7f0e3dc980a9a8bb26ad7375ab24a64967e3e19b
dastasoft <dastasoft@protonmail.com>
2020-03-16 22:40:02 +0100

    stepper

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,12 @@ import useQuestions from '../hooks/useQuestions';
 import UserContext from '../context/UserContext';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { MobileStepper } from '@material-ui/core';
 
 import './index.css';
 
 const IndexPage = () => {
-  const [questionId, setQuestionId] = useState(7);
+  const [questionId, setQuestionId] = useState(0);
   const [user, setUser] = useState('');
   const questionComponents = useQuestions(() => setQuestionId(questionId + 1));
 
@@ -17,6 +18,12 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" />
       <UserContext.Provider value={{ user, setUser }}>
+        <MobileStepper
+          variant="dots"
+          steps={questionComponents.length}
+          position="static"
+          activeStep={questionId}
+        />
         {questionComponents[questionId]}
       </UserContext.Provider>
     </Layout>
